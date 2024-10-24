432539ab783707bb1dc7b32a9ed41d0beaa4f17c
abhishekpatil4 <abhishekpersonal2@gmail.com>
2024-10-24 18:36:02 +0530

    fix: initiate connection data attribute name

--- src/app/api/newentityviaapi/route.js
+++ src/app/api/newentityviaapi/route.js
@@ -17,11 +17,12 @@ export async function POST(request) {
         const connectedAccount = await composio.connectedAccounts.initiate({
             integrationId: appIntegrationIds[appName],
             userUuid: newUserId,
-            connected_account_params:{
-          "admin_api_access_token": admin_api_access_token,
-          "shop": shopSubDomain
-          }
+            data:{
+                "admin_api_access_token": admin_api_access_token,
+                "shop": shopSubDomain
+            }
         });
+        console.log("\n\nconnectedAccount.connectionStatus :: ", connectedAccount);
         return NextResponse.json({
             authenticated: connectedAccount.connectionStatus === "ACTIVE" ? true : false,
         }, { status: 200});
